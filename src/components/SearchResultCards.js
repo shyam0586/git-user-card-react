3ab23042170de68cb4295ac8dc727ce3475ec4f5
shyam0586 <shyam0586@gmail.com>
2019-11-13 19:03:29 +0530

    css fixes added to search result cards for height alignment

--- src/components/SearchResultCards.js
+++ src/components/SearchResultCards.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { updateCurrentClickCard } from "../actions";
-
+import './SearchResultCards.css';
 
 class SearchResultCards extends Component {   
     handleClick(i){
@@ -12,17 +12,17 @@ class SearchResultCards extends Component {
             return (             
                 <div className = "row">
                     {this.props.searchResultArr.map((git, index) => {                        
-                        return (<div key={index} className="col-lg-3">
+                        return (<div key={index} className="cardContainer col-lg-3">
                         <img src={git.avatar_url} className="card-img-top" alt="avatar"/>
                         <div className="card-body">
                           <h5 className="card-title">{git.name}</h5>
                           <p className="card-text"><b>Company</b> : {git.company}</p>
                           <p className="card-text"><b>Location</b> : {git.location}</p>
-                          <p className="card-text"><b>No of Repos </b> : {git.public_repos}</p>                                                    
-                          <button type="button" onClick={this.handleClick.bind(this,index)} className="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">
+                          <p className="card-text"><b>No of Repos </b> : {git.public_repos}</p>                                                                              
+                        </div>
+                        <button type="button" onClick={this.handleClick.bind(this,index)} className="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">
                                 View More Details
                           </button>
-                        </div>
                       </div>)
                       })}
                 </div>
